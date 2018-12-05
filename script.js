4a5ac02d0d2ad7e69ff7f99e120893cd6b6a6fb5
John Wade Linatoc <44657508+johnlinatoc@users.noreply.github.com>
2018-12-04 22:20:02 -0600

    var items removed

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var button = document.getElementById("enter");
 var input = document.getElementById("userInput");
 var ul = document.querySelector("ul")
 var deleteBtns = document.getElementsByClassName("delete");
-var items = ul.getElementsByTagName("li");
+
 
 
 
